3fd0989f8009a2cd77797ff64e3a8a43366bf0c0
Nguyen Phong <phongvu3022@gmail.com>
2022-11-09 18:28:13 +0700

    abc

--- basic_web/settings.py
+++ basic_web/settings.py
@@ -143,7 +143,7 @@ STATIC_URL = '/static/'
 
 LOGIN_URL = '/users/login/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
